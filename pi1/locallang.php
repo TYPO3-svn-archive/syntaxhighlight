574f49fe9505ef262c2155e8d5e4588619a654b1
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-08 15:25:24 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/syntaxhighlight/trunk@10421 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ pi1/locallang.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Language labels for plugin "tx_skbookreview_pi1"
+ *
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'actionscript' => 'Actionscript',
+		'ada' => 'ADA',
+		'apache' => 'Apache',
+		'applescript' => 'Applescript',
+		'asm' => 'ASM',
+		'asp' => 'ASP',
+		'bash' => 'Bash',
+		'blitzbasic' => 'Blitzbasic',
+		'c' => 'C',
+		'c_mac' => 'C mac',
+		'caddcl' => 'caddcl',
+		'cadlisp' => 'cadlisp',
+		'cpp' => 'cpp',
+		'csharp' => 'C#',
+		'css' => 'CSS',
+		'd' => 'D',
+		'delphi' => 'Delphi',
+		'diff' => 'diff',
+		'div' => 'div',
+		'dos' => 'DOS',
+		'eiffel' => 'Eiffel',
+		'freebasic' => 'Freebasic',
+		'gml' => 'GML',
+		'html4strict' => 'HTML',
+		'ini' => 'INI',
+		'java' => 'Java',
+		'javascript' => 'Javascript',
+		'lisp' => 'Lisp',
+		'lua' => 'lua',
+		'matlab' => 'Matlab',
+		'mpasm' => 'mpASM',
+		'mysql' => 'mySQL',
+		'nsis' => 'nsis',
+		'objc' => 'objc',
+		'ocaml' => 'ocaml',
+		'ocaml-brief' => 'ocaml (brief)',
+		'oobas' => 'oobas',
+		'oracle8' => 'Oracle8',
+		'pascal' => 'Pascal',
+		'perl' => 'Perl',
+		'php' => 'PHP',
+		'php-brief' => 'PHP (brief)',
+		'python' => 'Python',
+		'qbasic' => 'QBasic',
+		'ruby' => 'ruby',
+		'scheme' => 'Scheme',
+		'sqlbasic' => 'SYLbasic',
+		'smarty' => 'Smarty',
+		'sql' => 'SQL',
+		'vb' => 'VB',
+		'vbnet' => 'vb.net',
+		'vhdl' => 'Vhdl',
+		'visualfoxpro' => 'Visual FoxPro',
+		'xml' => 'XML',	
+		'typoscript' => 'TypoScript',	
+		
+		'extlabel' => 'Code Highlight - Options',
+		'label' => 'Label:',
+		'lang' => 'Language:',
+		'code' => 'Code:',
+		'numbers' => 'Line Numbers:',
+		'width' => 'Width:',
+		'height' => 'Height:',
+	),
+	'dk' => Array (
+	),
+	'de' => Array (
+		'extlabel' => 'Code Highlight - Optionen',
+		'label' => 'Beschriftung:',
+		'lang' => 'Sprache:',
+		'code' => 'Code:',
+		'numbers' => 'Zeilennummern:',
+		'width' => 'Breite:',
+		'height' => 'Höhe:',
+	),
+	'no' => Array (
+	),
+	'it' => Array (
+	),
+	'fr' => Array (
+	),
+	'es' => Array (
+	),
+	'nl' => Array (
+	),
+	'cz' => Array (
+	),
+	'pl' => Array (
+	),
+	'si' => Array (
+	),
+	'fi' => Array (
+	),
+	'tr' => Array (
+	),
+	'se' => Array (
+	),
+	'pt' => Array (
+	),
+	'ru' => Array (
+	),
+	'ro' => Array (
+	),
+	'ch' => Array (
+	),
+	'sk' => Array (
+	),
+	'lt' => Array (
+	),
+	'is' => Array (
+	),
+	'hr' => Array (
+	),
+	'hu' => Array (
+	),
+	'gl' => Array (
+	),
+	'th' => Array (
+	),
+	'gr' => Array (
+	),
+	'hk' => Array (
+	),
+	'eu' => Array (
+	),
+	'bg' => Array (
+	),
+	'br' => Array (
+	),
+	'et' => Array (
+	),
+	'ar' => Array (
+	),
+	'he' => Array (
+	),
+	'ua' => Array (
+	),
+	'lv' => Array (
+	),
+	'jp' => Array (
+	),
+	'vn' => Array (
+	),
+	'ca' => Array (
+	),
+	'ba' => Array (
+	),
+	'kr' => Array (
+	),
+	'eo' => Array (
+	),
+	'my' => Array (
+	),
+);
+?>
