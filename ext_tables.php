bc9e61a6a7ab959ef6e7569907bf6cfaaae3d908
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-08 17:35:36 +0000

    move flexform to own dir
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/syntaxhighlight/trunk@10433 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -6,7 +6,7 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='la
 #pi_flexform
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
 # Wir definieren die Datei, die unser Flexform Schema enthält
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_code.xml');
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform/flexform_code.xml');
 
 t3lib_extMgm::addPlugin(Array('LLL:EXT:syntaxhighlight/language/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
