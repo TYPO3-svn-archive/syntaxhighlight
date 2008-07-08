dc96f26e89fffe10e87dc36aa16bd6efdf554056
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-08 16:56:42 +0000

    language file cleanup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/syntaxhighlight/trunk@10425 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -8,10 +8,7 @@ $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_fle
 # Wir definieren die Datei, die unser Flexform Schema enthält
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_code.xml');
 
-
-
-t3lib_extMgm::addPlugin(Array('LLL:EXT:syntaxhighlight/locallang_db.php:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-
+t3lib_extMgm::addPlugin(Array('LLL:EXT:syntaxhighlight/language/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","CodeBox");
 
