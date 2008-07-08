3a91b60f8115e458951c9d3ef39c932de4b98d56
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-08 21:30:54 +0000

    ext_localconf.php cleanup, added static templates and css
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/syntaxhighlight/trunk@10452 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,30 +1,17 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-  ## Extending TypoScript from static template uid=43 to set up userdefined tag:
-t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
-	tt_content.CSS_editor.ch.tx_syntaxhighlight_controller = < plugin.tx_syntaxhighlight_controller.CSS_editor
-',43);
-
-
-t3lib_extMgm::addPItoST43($_EXTKEY,'controller/class.tx_syntaxhighlight_controller.php','_controller','list_type',0);
-
-/**
- * Registering the Definition List plugin for htmlArea RTE (rtehtmlarea):
- */
-
+	// Registering the Definition List plugin for htmlArea RTE (rtehtmlarea):
 $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['Codebox'] = array();
 $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['Codebox']['objectReference'] = 'EXT:'.$_EXTKEY.'/Codebox/class.tx_rtehtmlarea_codebox.php:&tx_rtehtmlarea_codebox';
 
-$_EXTCONF = unserialize($_EXTCONF);    // unserializing the configuration so we can use it here:
-
-$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['Codebox']['addIconsToSkin'] = $_EXTCONF['addIconsToSkin'] ? $_EXTCONF['addIconsToSkin'] : 1;
-$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['Codebox']['disableInFE'] =  $_EXTCONF['disableInFE'] ? $_EXTCONF['disableInFE'] : 0;
+$extConf = unserialize($_EXTCONF);    // unserializing the configuration so we can use it here:
+$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['Codebox']['addIconsToSkin'] = $extConf['addIconsToSkin'] ? $extConf['addIconsToSkin'] : 1;
+$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['Codebox']['disableInFE'] =  $extConf['disableInFE'] ? $extConf['disableInFE'] : 0;
 
 	// Additional RTE transformation configuration
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/res/pageTSConfig.txt">');
 
 	// register ajax call
 $TYPO3_CONF_VARS['BE']['AJAX']['tx_syntaxhighlight_controller::highlightRTE'] = 'typo3conf/ext/syntaxhighlight/controller/class.tx_syntaxhighlight_controller.php:tx_syntaxhighlight_controller->highlightRTE';
-
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -1,19 +1,19 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_controller']='layout,select_key,pages,recursive';
-#pi_flexform
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_controller']='pi_flexform';
-# Wir definieren die Datei, die unser Flexform Schema enthält
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_controller'] = 'layout,select_key,pages,recursive';
+
+	// flexform
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_controller'] = 'pi_flexform';
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_controller', 'FILE:EXT:'.$_EXTKEY.'/flexform/flexform_code.xml');
 
-t3lib_extMgm::addPlugin(Array('LLL:EXT:syntaxhighlight/language/db.xml:tt_content.list_type_controller', $_EXTKEY.'_controller'),'list_type');
+t3lib_extMgm::addPlugin(Array('LLL:EXT:syntaxhighlight/language/db.xml:tt_content.list_type_controller', $_EXTKEY.'_controller'), 'list_type');
 
-t3lib_extMgm::addStaticFile($_EXTKEY,"controller/static/","CodeBox");
+t3lib_extMgm::addStaticFile($_EXTKEY, 'controller/static/', 'Syntax Highlighter');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'controller/static/css/', 'Syntax Highlighter css');
 
-if (TYPO3_MODE=="BE")    {
-    include_once(t3lib_extMgm::extPath($_EXTKEY).'api/class.syntaxhighlightAPI.php');   
+if (TYPO3_MODE=='BE') {
+	include_once(t3lib_extMgm::extPath($_EXTKEY).'api/class.syntaxhighlightAPI.php');   
 }  
-
 ?>
