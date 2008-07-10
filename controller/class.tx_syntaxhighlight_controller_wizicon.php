29014dac99dfe6637a2cb8dae450a8c9be77b6dd
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-10 14:10:17 +0000

    DOH!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/syntaxhighlight/trunk@10509 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ controller/class.tx_syntaxhighlight_controller_wizicon.php
@@ -0,0 +1,53 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*  
+*  (c) 2004 Michiel Roos (typo3@meyson.nl)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is 
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+* 
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+* 
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/** 
+ * Class that adds the wizard icon.
+ *
+ * @author	Michiel Roos <typo3@meyson.nl>
+ */
+class tx_syntaxhighlight_controller_wizicon {
+	function proc($wizardItems)	{
+		global $LANG;
+		$LL = $this->includeLocalLang();
+
+		$wizardItems['plugins_tx_syntaxhighlight_controller'] = array(
+			'icon' => t3lib_extMgm::extRelPath('syntaxhighlight').'icon/ce_wiz.gif',
+			'title' => $LANG->getLLL('title', $LL),
+			'description' => $LANG->getLLL('description', $LL),
+			'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=syntaxhighlight_controller'
+		);
+
+		return $wizardItems;
+	}
+
+	function includeLocalLang()	{
+		$llFile = t3lib_extMgm::extPath('syntaxhighlight').'language/db.xml';
+		return t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/syntaxhighlight/controller/class.tx_syntaxhighlight_controller_wizicon.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/syntaxhighlight/controller/class.tx_syntaxhighlight_controller_wizicon.php']);
+}
+?>
